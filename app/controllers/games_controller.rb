e22b520a12a226d776634c6cfd2a9d34c7619fa4
Michael Xu <xu.michaelxia@gmail.com>
2018-04-02 16:26:30 +0800

    completed the game with scoring mechanism

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,41 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     @letters = []
     10.times do
-      @letters << ("a".."z").to_a.sample;
+      @letters << ("a".."z").to_a.sample.upcase;
     end
+    @start_time = Time.now
+  end
+
+  def included?(guess, grid)
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
+
+  def compute_score(attempt, time_taken)
+    time_taken > 60.0 ? 0 : attempt.size * (1.0 - time_taken / 60.0)
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
   end
 
   def score
+    word = params[:word].upcase
+    @letters = params[:letters].split(' ')
+    if included?(word, @letters)
+      if english_word?(word)
+        score = compute_score(word, params[:time].to_f)
+        @message = [score.round, "Well done!"]
+      else
+        @message = [0, "Sorry but #{word} is not an english word."]
+      end
+    else
+      @message = [0, "Sorry but #{word} can't be built out of the above letters."]
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'score', to: 'games#score', as: :score
+  # get 'score', to: 'games#score', as: :score
   get 'new', to: 'games#new', as: :new
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'score', to: 'games#score', as: :score
 end
